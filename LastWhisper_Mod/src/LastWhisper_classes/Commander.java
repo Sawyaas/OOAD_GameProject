9e4620b71346e8df0bbbe750834b95b242eca820
MartinDewy <martin.r.dewhurst@students.ittralee.ie>
2015-12-04 17:27:18 +0000

    Created Commander class, added attributes String name, int charisma, int strength, int warAbility  all attributes are private at the time of creation. Created constructor from these attributes.

---
+++ LastWhisper_Mod/src/LastWhisper_classes/Commander.java
@@ -0,0 +1,18 @@
+package LastWhisper_classes;
+
+/**
+ * Created by Martin on 04/12/2015.
+ */
+public class Commander {
+   private String name;
+   private int charisma;
+   private int strength;
+   private int warAbility;
+
+    public Commander(String name, int charisma, int strength, int warAbility) {
+        this.name = name;
+        this.charisma = charisma;
+        this.strength = strength;
+        this.warAbility = warAbility;
+    }
+}
