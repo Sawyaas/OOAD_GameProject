a1a11a18946285163f03be1a9a6c611256ced5f3
MartinDewy <martin.r.dewhurst@students.ittralee.ie>
2015-12-04 16:39:29 +0000

    Created Module in OOAD_GameProject, Within that module created new Package: LastWhisper_classes. Then created Army class with attributes numOFSwordmen, numOfDogs, numOfSpearmen, numOfCavalries, numOfSiegeEquipment, numOfArchers. Selected all attributes and created constructor.

---
+++ LastWhisper_Mod/src/LastWhisper_classes/Army.java
@@ -0,0 +1,22 @@
+package LastWhisper_classes;
+
+/**
+ * Created by Martin on 04/12/2015.
+ */
+public class Army {
+    private int numOfSwordmen;
+    private int numOfDogs;
+    private int numOfSpearmen;
+    private int numOfCavalries;
+    private int numOfSiegeEquipment;
+    private int numOfArchers;
+
+    public Army(int numOfSwordmen, int numOfDogs, int numOfSpearmen, int numOfCavalries, int numOfSiegeEquipment, int numOfArchers) {
+        this.numOfSwordmen = numOfSwordmen;
+        this.numOfDogs = numOfDogs;
+        this.numOfSpearmen = numOfSpearmen;
+        this.numOfCavalries = numOfCavalries;
+        this.numOfSiegeEquipment = numOfSiegeEquipment;
+        this.numOfArchers = numOfArchers;
+    }
+}
