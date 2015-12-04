8bd09a2ae792a7812060809dd4c359ed4e8dd4b5
MartinDewy <martin.r.dewhurst@students.ittralee.ie>
2015-12-04 17:21:56 +0000

    Created Resources class, added attributes int foodQuantity, int woodQuantity, int stoneQuantity  all attributes are private at the time of creation. Created constructor from these attributes.

---
+++ LastWhisper_Mod/src/LastWhisper_classes/Resources.java
@@ -0,0 +1,16 @@
+package LastWhisper_classes;
+
+/**
+ * Created by Martin on 04/12/2015.
+ */
+public class Resources {
+    private int foodQuantity;
+    private int woodQuantity;
+    private int stoneQuantity;
+
+    public Resources(int foodQuantity, int woodQuantity, int stoneQuantity) {
+        this.foodQuantity = foodQuantity;
+        this.woodQuantity = woodQuantity;
+        this.stoneQuantity = stoneQuantity;
+    }
+}
