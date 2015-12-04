603cc2f349db4bee2d2df45e1ae9fac886fb398b
MartinDewy <martin.r.dewhurst@students.ittralee.ie>
2015-12-04 17:31:34 +0000

    Created CommanderControl class, added Super Constructor  Created constructor from these attributes.

---
+++ LastWhisper_Mod/src/LastWhisper_classes/CommanderControl.java
@@ -0,0 +1,10 @@
+package LastWhisper_classes;
+
+/**
+ * Created by Martin on 04/12/2015.
+ */
+public class CommanderControl extends Commander {
+    public CommanderControl(String name, int charisma, int strength, int warAbility) {
+        super(name, charisma, strength, warAbility);
+    }
+}
