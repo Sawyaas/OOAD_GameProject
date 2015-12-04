206ccd013b25b2a1f967be8af5a5181446a99e24
MartinDewy <martin.r.dewhurst@students.ittralee.ie>
2015-12-04 17:19:30 +0000

    Created Unit class, added attributes integers attachValue, defenceValue  and String name all attributes are private at the time of creation. Created constructor from these attributes.

---
+++ LastWhisper_Mod/src/LastWhisper_classes/Unit.java
@@ -0,0 +1,16 @@
+package LastWhisper_classes;
+
+/**
+ * Created by Martin on 04/12/2015.
+ */
+public class Unit {
+    private String name;
+    private int attackValue;
+    private int defenceValue;
+
+    public Unit(String name, int attackValue, int defenceValue) {
+        this.name = name;
+        this.attackValue = attackValue;
+        this.defenceValue = defenceValue;
+    }
+}
