65bc1f286730f76dc069cdf120c598c31bf62835
t00161717 <martinireland777@gmail.com>
2015-12-07 14:59:17 +0000

    Added methods in Base.java Class: displayInfo(), transferUnits(), destroy(), attack(),

--- LastWhisper_Mod/src/LastWhisper_classes/Commander.java
+++ LastWhisper_Mod/src/LastWhisper_classes/Commander.java
@@ -4,15 +4,7 @@ package LastWhisper_classes;
  * Created by Martin on 04/12/2015.
  */
 public class Commander {
-   //private String name;
-   //private int charisma;
-   //private int strength;
-   //private int warAbility;
 
-    public Commander( int charisma, int strength, int warAbility) {
-        //this.name = name;
-        //this.charisma = charisma;
-        //this.strength = strength;
-        //this.warAbility = warAbility;
-    }
+
+
 }
