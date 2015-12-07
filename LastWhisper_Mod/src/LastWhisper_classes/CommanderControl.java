7c9e1d743cab98c8e2d5d01b4e697f76f2b8b0ea
t00161717 <martinireland777@gmail.com>
2015-12-07 14:17:16 +0000

    Changed CommanderControl  class after talking to team, CommanderControl  was extending Commander in my draft code classes but CommanderControl  does not extend, also removed super consdtructor from CommanderControl  class.

--- LastWhisper_Mod/src/LastWhisper_classes/CommanderControl.java
+++ LastWhisper_Mod/src/LastWhisper_classes/CommanderControl.java
@@ -3,8 +3,8 @@ package LastWhisper_classes;
 /**
  * Created by Martin on 04/12/2015.
  */
-public class CommanderControl extends Commander {
+public class CommanderControl  {
     public CommanderControl(String name, int charisma, int strength, int warAbility) {
-        super(name, charisma, strength, warAbility);
+
     }
 }
