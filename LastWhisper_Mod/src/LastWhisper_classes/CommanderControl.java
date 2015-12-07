0199363ea6a55ae54809491279ec1211f1841c58
t00161717 <martinireland777@gmail.com>
2015-12-07 15:47:34 +0000

    Changed Draft Class CommanderControl to ENUM CommanderControl and added AI , PLAYER Fields

--- LastWhisper_Mod/src/LastWhisper_classes/CommanderControl.java
+++ LastWhisper_Mod/src/LastWhisper_classes/CommanderControl.java
@@ -3,8 +3,7 @@ package LastWhisper_classes;
 /**
  * Created by Martin on 04/12/2015.
  */
-public class CommanderControl  {
-    public CommanderControl() {
-
-    }
+public enum CommanderControl  {
+    AI,PLAYER;
 }
+
