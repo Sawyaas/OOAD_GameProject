a8a28c154fc21148eaab677c99979f75e33b7f93
Sawyaas <sawyaas@gmail.com>
2015-12-09 18:34:22 +0000

    Implement a simulation of UC_6.0 in the Main class.

--- App/src/ie/ittralee/Main.java
+++ App/src/ie/ittralee/Main.java
@@ -1,8 +1,7 @@
 package ie.ittralee;
 
-import ie.ittralee.entities.Commander;
-import ie.ittralee.entities.CommanderControl;
-import ie.ittralee.entities.Resources;
+import ie.ittralee.entities.Base;
+import ie.ittralee.entities.Map;
 
 /**
  * Created by Simon on 09/12/2015.
@@ -11,5 +10,20 @@ public class Main {
     public static void main(String[] args) {
         /* Initialisations required before starting the User Case */
 
+        GameInfo gameInfo = GameInfo.getInstance();
+        Map map = gameInfo.getMap();
+        Base playerBase = gameInfo.getBaseList().get(0);
+        Base ennemyBase = gameInfo.getBaseList().get(1);
+
+        /* UC_6.0 Simulation */
+
+        System.out.println("Player goes to the map.");
+        map.display();
+
+        System.out.println("Player check the enemy base's info.");
+        ennemyBase.displayInfo();
+
+        System.out.println("Player attack the enemy base.");
+        ennemyBase.attack(playerBase);
     }
 }
