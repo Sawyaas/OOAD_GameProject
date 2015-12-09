18390ff17570fd58921a6c7459be9074c0d7f539
Sawyaas <sawyaas@gmail.com>
2015-12-09 16:20:44 +0000

    Improve code quality in Commander and GameInfo classes

--- App/src/ie/ittralee/GameInfo.java
+++ App/src/ie/ittralee/GameInfo.java
@@ -3,8 +3,8 @@ package ie.ittralee;
 import ie.ittralee.entities.Unit;
 import ie.ittralee.entities.UnitName;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Simon on 09/12/2015.
@@ -13,28 +13,32 @@ public class GameInfo {
     /* --- ATTRIBUTES --- */
 
     public static GameInfo instance;
-    private final Unit[] units;
+    private final Map<UnitName, Unit> units;
 
 
     /* --- CONSTRUCTORS --- */
 
     private GameInfo() {
-        units = new Unit[6];
-        units[0] = new Unit(UnitName.SWORDMEN, 6, 4);
-        units[1] = new Unit(UnitName.DOGS, 4, 2);
-        units[2] = new Unit(UnitName.SPEARMEN, 5, 5);
-        units[3] = new Unit(UnitName.CAVALRIES, 7, 1);
-        units[4] = new Unit(UnitName.SIEGE_EQUIPMENTS, 10, 0);
-        units[5] = new Unit(UnitName.ARCHERS, 2, 8);
+        units = new HashMap<UnitName, Unit>();
+        units.put(UnitName.SWORDMEN, new Unit(UnitName.SWORDMEN, 6, 4));
+        units.put(UnitName.DOGS, new Unit(UnitName.DOGS, 4, 2));
+        units.put(UnitName.SPEARMEN, new Unit(UnitName.SPEARMEN, 5, 5));
+        units.put(UnitName.CAVALRIES, new Unit(UnitName.CAVALRIES, 7, 1));
+        units.put(UnitName.SIEGE_EQUIPMENTS, new Unit(UnitName.SIEGE_EQUIPMENTS, 10, 0));
+        units.put(UnitName.ARCHERS, new Unit(UnitName.ARCHERS, 2, 8));
     }
 
 
     /* --- GETTERS --- */
 
-    public GameInfo getInstance() {
+    public static GameInfo getInstance() {
         if (instance == null)
             instance = new GameInfo();
 
         return instance;
+    }
+
+    public Map<UnitName, Unit> getUnits() {
+        return units;
     }
 }
--- App/src/ie/ittralee/entities/Commander.java
+++ App/src/ie/ittralee/entities/Commander.java
@@ -1,21 +1,31 @@
 package ie.ittralee.entities;
 
+import java.util.*;
+
 /**
  * Created by Martin on 04/12/2015.
  */
 public class Commander {
+    /* --- ATTRIBUTES --- */
 
-    public Resources getStock;
-    //public Resources transferResources;  Need help with this one Simon and Niamh
+    private Resources stock;
 
 
-    public Commander(Resources getStock) {
-        this.getStock = getStock;
+    /* --- CONSTRUCTORS --- */
+
+    public Commander() { }
+
+
+    /* --- GETTERS --- */
+
+    public Resources getStock() {
+        return stock;
     }
 
 
+    /* --- METHODS --- */
 
-
-
-
+    public static void transferResources(Commander from, Commander to, java.util.Map<ResourceName, Integer> resources) {
+        // TODO
+    }
 }
