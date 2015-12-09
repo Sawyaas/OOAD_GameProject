643ef3683ff0df7eab4b1962844e2608fb5c3079
Sawyaas <sawyaas@gmail.com>
2015-12-09 16:46:52 +0000

    Implement the attack method of Base class.

--- App/src/ie/ittralee/entities/Base.java
+++ App/src/ie/ittralee/entities/Base.java
@@ -1,47 +1,136 @@
 package ie.ittralee.entities;
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Martin on 04/12/2015.
  */
 public class Base {
-    private int attack;
-    private int defence;
-    private Base displayInfo;
-    private Base transferUnits;
-    private Base destroy;
+    /* --- ATTRIBUTES --- */
 
-    public Base(int attack, int defence, Base displayInfo, Base transferUnits, Base destroy) {
-        this.attack = attack;
-        this.defence = defence;
-        this.displayInfo = displayInfo;
-        this.transferUnits = transferUnits;
-        this.destroy = destroy;
+    private Commander commander;
+    private Army army;
 
+
+    /* --- CONSTRUCTORS --- */
+
+    public Base() { }
+
+
+    /* --- GETTERS --- */
+
+    public Commander getCommander() {
+        return commander;
     }
 
-    public Base getDisplayInfo() {
-        return displayInfo;
+    public Army getArmy() {
+        return army;
     }
 
-    public Base getTransferUnits() {
-        return transferUnits;
+
+    /* --- METHODS --- */
+
+    public void attack(Base attacker) {
+        /* Calculate the total attack value of the offensive army
+         * and the total defence value of the defencive army */
+
+        int attackValue = attacker.army.sumAttackValues();
+        int defenceValue = this.army.sumDefenceValues();
+
+        /* Determine the winner by comparing the attack value
+         * and the defence value */
+
+        boolean attackerWon;
+        if (attackValue >= defenceValue + defenceValue * 0.5){   // At least 50% higher
+            // Offensive side automatically wins
+            attackerWon = true;
+        } else if (defenceValue >= attackValue + attackValue * 0.5) {    // At least 50% higher
+            // Defensive side automatically wins
+            attackerWon = false;
+        } else {
+            double randomValue = Math.random();
+            if (randomValue < 0.5) {
+                // Defensive side randomly wins
+                attackerWon = false;
+            } else {
+                // Offensive side randomly wins
+                attackerWon = true;
+            }
+        }
+
+        /* Give a random part of the resources and of the remaining units of the losing side
+         * to the winning side */
+
+        Base winningSide = (attackerWon) ? attacker : this;
+        Base losingSide = (attackerWon) ? this : attacker;
+        Resources looserStock = losingSide.commander.getStock();
+        Army looserArmy = losingSide.army;
+        Map<ResourceName, Integer> resourcesGiven = new HashMap<ResourceName, Integer>();
+        Map<UnitName, Integer> unitsGiven = new HashMap<UnitName, Integer>();
+
+        int percentage = (int) Math.random() * 10 + 11;  // Random % between 11 and 20
+
+        int food = looserStock.getFoodQuantity() * (percentage/100);
+        int stone = looserStock.getStoneQuantity() * (percentage/100);
+        int wood = looserStock.getWoodQuantity() * (percentage/100);
+
+        resourcesGiven.put(ResourceName.FOOD, food);
+        resourcesGiven.put(ResourceName.STONE, stone);
+        resourcesGiven.put(ResourceName.WOOD, wood);
+
+        int swordmen = looserArmy.getNumOfSwordmen() * (percentage/100);
+        int dogs = looserArmy.getNumOfDogs() * (percentage/100);
+        int spearmen = looserArmy.getNumOfSpearmen() * (percentage/100);
+        int cavalries = looserArmy.getNumOfCavalries() * (percentage/100);
+        int siegeEquipments = looserArmy.getNumOfSiegeEquipment() * (percentage/100);
+        int archers = looserArmy.getNumOfArchers() * (percentage/100);
+
+        unitsGiven.put(UnitName.SWORDMEN, swordmen);
+        unitsGiven.put(UnitName.DOGS, dogs);
+        unitsGiven.put(UnitName.SPEARMEN, spearmen);
+        unitsGiven.put(UnitName.CAVALRIES, cavalries);
+        unitsGiven.put(UnitName.SIEGE_EQUIPMENTS, siegeEquipments);
+        unitsGiven.put(UnitName.ARCHERS, archers);
+
+        Commander.transferResources(losingSide.commander, winningSide.commander, resourcesGiven);
+        Base.transferUnits(losingSide, winningSide, unitsGiven);
+
+        /* If the defensive side has lost, its base is destroyed */
+
+        if (attackerWon)
+            this.destroy();
+
+        /* Show the final battle screen */
+
+        String result = (attackerWon) ? "won" : "lost";
+        String finalOutput =
+                "You have " + result + "!\n" +
+                "Resources " + result + ":\n" +
+                "\t- " + food + " Food\n" +
+                "\t- " + stone + " Stone\n" +
+                "\t- " + wood + " Wood\n" +
+                "Units " + result + ":\n" +
+                "\t- " + swordmen + " Swordmen\n" +
+                "\t- " + dogs + " Dogs\n" +
+                "\t- " + spearmen + " Spearmen\n" +
+                "\t- " + cavalries + " Cavalries\n" +
+                "\t- " + siegeEquipments + " Siege Equipments\n" +
+                "\t- " + archers + " Archers";
+        System.out.println(finalOutput);
     }
 
-    public int getAttack() {
-        return attack;
+    public void destroy() {
+        // TODO
     }
 
-    public int getDefence() { return defence; }
-
-    public Base getDestroy() { return destroy; }
-
-    public void setDestroy(Base destroy) { this.destroy = destroy; }
-
-    public void setAttack(int attack) {
-        this.attack = attack;
+    public void displayInfo() {
+        // TODO
     }
 
-    public void setDefence(int defence) {
-        this.defence = defence;
+    public static void transferUnits(Base from, Base to, java.util.Map<UnitName, Integer> units) {
+        // TODO
     }
 }
