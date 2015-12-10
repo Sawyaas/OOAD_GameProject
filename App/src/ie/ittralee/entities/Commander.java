f79b4e001217c73bfcaa95458d324c18a98e05fc
t00158725 <nmmoylan@gmail.com>
2015-12-10 14:01:27 +0000

    transferResources Method created allowing for the transfer of stock between the winning and loosing commander

--- App/src/ie/ittralee/entities/Commander.java
+++ App/src/ie/ittralee/entities/Commander.java
@@ -34,6 +34,37 @@ public class Commander {
     /* --- METHODS --- */
 
     public static void transferResources(Commander from, Commander to, java.util.Map<ResourceName, Integer> resources) {
-        // TODO
+
+
+        int food =  from.getStock().getFoodQuantity() - resources.get(ResourceName.FOOD);
+        from.getStock().setFoodQuantity(food);
+
+        System.out.print(food);
+
+        int wood = from.getStock().getWoodQuantity() - resources.get(ResourceName.WOOD);
+        from.getStock().setWoodQuantity(wood);
+
+        System.out.print(wood);
+
+        int stone = from.getStock().getStoneQuantity() - resources.get(ResourceName.STONE);
+        from.getStock().setStoneQuantity(stone);
+
+        System.out.print(stone);
+
+        int foodTo = to.getStock().getFoodQuantity() +  resources.get(ResourceName.FOOD);
+        to.getStock().setFoodQuantity(foodTo);
+
+        System.out.print(foodTo);
+
+        int woodTo = to.getStock().getWoodQuantity() + resources.get(ResourceName.WOOD);
+        to.getStock().setWoodQuantity(woodTo);
+
+        System.out.print(woodTo);
+
+        int stoneTo = to.getStock().getStoneQuantity() + resources.get(ResourceName.STONE);
+        to.getStock().setStoneQuantity(stoneTo);
+
+        System.out.print(stoneTo);
+
     }
 }
